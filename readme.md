b68d1503beb978a74cd44e4b8a229452d910e15b
Oluwole <oluwole@Oluwoles-MacBook-Pro.local>
2020-12-10 05:41:29 +0100

    added call to third party api for kyc verification

--- readme.md
+++ readme.md
@@ -1,3 +1,21 @@
 # KNOW_YOUR_CUSTOMER
 This apps helps to know beneficiary/customer kyc level. 
 It takes in the details of the users and check if they are who they claim to be.
+
+```
+Node
+typescript
+Libaries : prettier, mocha, axios
+```
+##  To Install
+
+- Download or clone
+- Open terminal inside the root directory of clone folder
+- Type npm install to install all dependencies
+- npm run build
+- npm start to run the app
+- npm run dev to run development environment
+- npm run test to run the test suites on the app(NODE_ENV='test')
+
+##  AUTHOR
+Oluwole
--- src/controller/checkCustomer.ts
+++ src/controller/checkCustomer.ts
@@ -1,3 +1,6 @@
+import { config } from '../config/index';
+
+
 //makes sure that data input to function complies with this shape
 interface kycRequestBody {
   birthDate: string;
@@ -9,18 +12,26 @@ interface kycRequestBody {
   expiryDate?: string;
 }
 
-//This function takes in user details to check their kyc(know your customer)
-export const checkCustomer = ({ validateUser }) => async (
-  data: kycRequestBody
-): Promise<{}> => {
+/**
+   * @name checkCustomer
+   * @description This function takes in user details to check their kyc(know your customer)
+   * @param interface kycRequestBody  the transaction id
+   * @returns {} an empty promise when complete
+   */
+  
+export const checkCustomer = ({
+  validateUser,
+  axios,
+  VerifyDocumentError,
+}) => async (data: kycRequestBody): Promise<{}> => {
   try {
-      
     //check user details to validate against set constrains
     const { error } = validateUser(data);
     //throw error in cases of contraints violation
     if (error) throw new Error(error);
 
     //api request body
+    // I assumed that given name is firstname, familyName is lastName
     const requestBody = {
       birthDate: data.birthDate,
       givenName: data.firstName,
@@ -30,7 +41,33 @@ export const checkCustomer = ({ validateUser }) => async (
       stateOfIssue: data.stateOfIssue,
       expiryDate: data.expiryDate,
     };
+    //set request headers
+    const headers = {
+      Authorization: `Bearer ${config.TEST_APIKEY}`,
+    };
+    //response body
+    const response = await axios.post(config.TEST_URL, requestBody, {
+      headers,
+    });
+    let kycResult: boolean = false;
+    if (response.data.verificationResultCode === 'N') {
+      kycResult = true;
+      return { kycResult };
+    }
+    if (response.data.verificationResultCode === 'Y') {
+      kycResult = false;
+      return { kycResult };
+    }
+    if (
+      response.data.verificationResultCode === 'D' ||
+      response.data.verificationResultCode === 'S'
+    ) {
+      throw new VerifyDocumentError({
+        code: '“D” or “S”',
+        message: 'Document Error” or “Server Error',
+      });
+    }
   } catch (error) {
-    console.log(error);
+    throw error;
   }
 };
